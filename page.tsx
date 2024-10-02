ca2e903a09447752f1715a0e079514d378087f46
maida277 <maida.intizar1998@gmail.com>
2024-10-02 16:25:50 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => {
+
+  return (
+    <body>
+
+
+                <div className="main">
+                             Hi,
+                             <br />
+                             This is <span>MAIDA INTIZAR</span>
+                             <br /> an enthusiastic Website Developer associated with <span>NEXT-JS</span>. |
+                </div>
+                  
+                <div className="main">
+                             <img src="image.png" alt="Dp" className="profile"/>
+                </div>
+
+
+  </body>
+
+
+
+  )
+
+};
+
+export default Home;
